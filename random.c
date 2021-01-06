e6ad2a038415892cd22cddc3a8b90f67bdda3f08
Archit Gupta <archigup@amazon.com>
2021-01-06 10:59:08 -0800

    Add mbedtls SHA-1

--- random.c
+++ random.c
@@ -1,12 +1,13 @@
 #include "FreeRTOS.h"
 #include "task.h"
+#include "mbedtls/sha1.h"
 
 /* Note that this MACRO should be used only with 32 bit numbers */
-#define ulROTATELEFT( a, b )   ( ( a << b ) || ( a >> ( 32 - b ) ) )
-#define ulROTATERIGHT( a, b )   ( ( a >> b ) || ( a << ( 32 - b ) ) )
+#define ulROTATELEFT( a, b )     ( ( a << b ) || ( a >> ( 32 - b ) ) )
+#define ulROTATERIGHT( a, b )    ( ( a >> b ) || ( a << ( 32 - b ) ) )
 
 /* Please define this number as a multiple of 4. */
-#define BYTES_IN_POOL   4
+#define BYTES_IN_POOL    4
 
 /* Make sure that this number is multiple of 4 */
 #if ( BYTES_IN_POOL & 0x03 ) > 0
@@ -14,72 +15,70 @@
 #endif
 
 /* Define however the Entropy pool should be. */
-union EntropyPool {
-	uint8_t ucBytes[BYTES_IN_POOL];
-	uint32_t ulWord[ BYTES_IN_POOL / 4 ];
+union EntropyPool
+{
+    uint8_t ucBytes[ BYTES_IN_POOL ];
+    uint32_t ulWord[ BYTES_IN_POOL / 4 ];
 };
 typedef union EntropyPool EntropyType_t;
 
 /* Define the FreeRTOS entropy pool to be used. */
-EntropyType_t  FreeRTOSEntropyPool;
-
-uint32_t ulSHA1( uint32_t input )
-{
-	uint32_t ulReturn = input;
-
-	return ulReturn;
-}
+EntropyType_t FreeRTOSEntropyPool;
 
 /* Initialise the RNG.
  * It can be seeded if required using value in xSeed and
  * by setting the xIsSeeded parameter to pdTRUE, else it will
  * be automatically seeded. */
-void vRNGInit( BaseType_t xIsSeeded, EntropyType_t xSeed )
+void vRNGInit( BaseType_t xIsSeeded,
+               EntropyType_t xSeed )
 {
-	if( xIsSeeded == pdTRUE )
-	{
-	    memcpy( &FreeRTOSEntropyPool, &xSeed, sizeof( xSeed ) );
-	}
-	else
-	{
-		TickType_t xTicks = xTaskGetTickCount();
-		EntropyType_t xLocalSeed;
+    if( xIsSeeded == pdTRUE )
+    {
+        memcpy( &FreeRTOSEntropyPool, &xSeed, sizeof( xSeed ) );
+    }
+    else
+    {
+        TickType_t xTicks = xTaskGetTickCount();
+        EntropyType_t xLocalSeed;
 
-		xLocalSeed.ulWord[0] = ulSHA1( ( uint32_t ) xTicks );
+        xLocalSeed.ulWord[ 0 ] = ( uint32_t ) xTicks;
 
-		memcpy( &FreeRTOSEntropyPool, &xLocalSeed, sizeof( xSeed ) );
-	}
+        memcpy( &FreeRTOSEntropyPool, &xLocalSeed, sizeof( xSeed ) );
+    }
 }
 
 /* Function to add entropy from an ISR. */
 void vAddBytesToPoolFromISR( BaseType_t xISRNumber )
 {
-	TickType_t xTicks = xTaskGetTickCountFromISR();
-	BaseType_t xLocalISRNumber = xISRNumber;
+    TickType_t xTicks = xTaskGetTickCountFromISR();
+    BaseType_t xLocalISRNumber = xISRNumber;
 
-	uint32_t ulNumber = ( ( uint32_t ) xTicks ^ ( uint32_t ) xLocalISRNumber );
+    uint32_t ulNumber = ( ( uint32_t ) xTicks ^ ( uint32_t ) xLocalISRNumber );
 }
 
 /* Function to add entropy from a non-ISR function. */
 void vAddBytesToPool()
 {
-	TickType_t xTicks = xTaskGetTickCount();
+    TickType_t xTicks = xTaskGetTickCount();
 }
 
 /* Function to get a random number from the SHA1 of the pool. */
-uint32_t ulGetRandomNumber(void)
+uint32_t ulGetRandomNumber( void )
 {
-	uint32_t ulReturn = 0;
+    uint32_t ulReturn = 0;
 
-	for( int i =0; i < ( sizeof( EntropyType_t ) / 4 ); i++ )
-		ulReturn ^= FreeRTOSEntropyPool.ulWord[i];
+    for( int i = 0; i < ( sizeof( EntropyType_t ) / 4 ); i++ )
+    {
+        ulReturn ^= FreeRTOSEntropyPool.ulWord[ i ];
+    }
 
-	ulReturn = ulSHA1( ulReturn );
+    ulReturn = xOWF( ulReturn ).l;
 
-	return ulReturn;
+    return ulReturn;
 }
 
-typedef struct xOWFRet {
+typedef struct xOWFRet
+{
     uint32_t l;
     uint32_t r;
 } xOWFRet_t;
@@ -87,24 +86,36 @@ typedef struct xOWFRet {
 static uint32_t ulRandState;
 
 /* Function to add entropy. */
-void vAddBytesToPool2(uint32_t ulEntropy)
+void vAddBytesToPool2( uint32_t ulEntropy )
 {
-	ulRandState = ulROTATELEFT( ulRandState, 1 ) ^ ulEntropy;
-    ulRandState ^= (uint32_t) xTaskGetTickCount();
+    ulRandState = ulROTATELEFT( ulRandState, 1 ) ^ ulEntropy;
+    ulRandState ^= ( uint32_t ) xTaskGetTickCount();
 }
 
-uint32_t ulGetRandomNumber2(void)
+uint32_t ulGetRandomNumber2( void )
 {
-	xOWFRet_t xOWFOutput;
-	xOWFOutput = xOWF(ulRandState);
-	ulRandState ^= xOWFOutput.l;
-	return xOWFOutput.r;
+    xOWFRet_t xOWFOutput;
+
+    xOWFOutput = xOWF( ulRandState );
+    ulRandState ^= xOWFOutput.l;
+    return xOWFOutput.r;
 }
 
-static xOWFRet xOWF(uint32_t input)
+static xOWFRet xOWF( uint32_t input )
 {
-    //placeholder
-    return 0;
+    union pcSHAOutput
+    {
+        unsigned char chars[ 20 ];
+        uint32_t int32[ 5 ];
+    }
+    output;
+
+    unsigned char pcSHAOutput[ 20 ];
+
+    mbedtls_sha1( ( *unsigned char )input, 4, output.chars );
+
+    /*placeholder */
+    return {
+               output.int32[ 0 ], output.int32[ 1 ]
+    };
 }
-
-
