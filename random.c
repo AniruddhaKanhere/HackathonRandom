d7ae88d30e8db42e939c06c4a3fba7e208264fd8
Aniruddha Kanhere <60444055+AniruddhaKanhere@users.noreply.github.com>
2021-01-05 17:47:13 -0800

    Update the file

--- random.c
+++ random.c
@@ -1,20 +1,60 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
+/* Make sure that this number is multiple of 4 */
 #define BYTES_IN_POOL   8
 
+#if ( BYTES_IN_POOL & 0x03 ) > 0
+    #error "BYTES_IN_POOL not a multiple of 4."
+#endif
 /* Note that this MACRO should be used only with 32 bit numbers */
 #define ulROTATELEFT( a, b )   ( ( a << b ) || ( a >> ( 32 - b ) ) )
 #define ulROTATERIGHT( a, b )   ( ( a >> b ) || ( a << ( 32 - b ) ) )
 
-struct EntropyPool {
+/* Define however the Entropy pool should be. */
+union EntropyPool {
 	uint8_t ucBytes[BYTES_IN_POOL];
+	uint32_t ulWord[ BYTES_IN_POOL / 4 ];
 };
+typedef union EntropyPool EntropyType_t;
+
+/* Define the FreeRTOS entropy pool to be used. */
+EntropyType_t  FreeRTOSEntropyPool;
+
+uint32_t ulSHA1( uint32_t input )
+{
+}
+
+/* Initialise the RNG.
+ * It can be seeded if required using value in xSeed and
+ * by setting the xIsSeeded parameter to pdTRUE, else it will
+ * be automatically seeded. */
+void vRNGInit( BaseType_t xIsSeeded, EntropyType_t xSeed )
+{
+	if( xIsSeeded == pdTRUE )
+	{
+	    memcpy( &FreeRTOSEntropyPool, &xSeed, sizeof( xSeed ) );
+	}
+	else
+	{
+		TickType_t xTicks = xTaskGetTickCount();
+		EntropyType_t xLocalSeed;
+
+		xLocalSeed.ulWord[0] = ulSHA1( ( uint32_t ) xTicks );
+
+		memcpy( &FreeRTOSEntropyPool, &xLocalSeed, sizeof( xSeed ) );
+	}
+}
 
 /* Function to add entropy from an ISR. */
 void vAddBytesToPoolFromISR( BaseType_t xISRNumber )
 {
 	TickType_t xTicks = xTaskGetTickCountFromISR();
+	BaseType_t xLocalISRNumber = xISRNumber;
+
+	uint32_t ulNumber = ( ( uint32_t ) xTicks ^ ( uint32_t ) xLocalISRNumber );
+
+
 }
 
 /* Function to add entropy from a non-ISR function. */
