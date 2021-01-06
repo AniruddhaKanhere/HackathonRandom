da0ff375bbe45c3909a496bfaefbe21cfbf90004
Archit Gupta <archigup@amazon.com>
2021-01-06 11:06:02 -0800

    Add other functions to header

--- random.h
+++ random.h
@@ -1,2 +1,5 @@
-
+void vRNGInit( BaseType_t xIsSeeded,
+               EntropyType_t xSeed );
+void vAddBytesToPoolFromISR( BaseType_t xISRNumber );
+void vAddBytesToPool( uint32_t ulEntropy );
 uint32_t ulGetRandomNumber( void );
