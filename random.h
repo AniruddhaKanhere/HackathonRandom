4b6287ff22467ffac9c6ebc040461e20f02dbc47
Aniruddha Kanhere <60444055+AniruddhaKanhere@users.noreply.github.com>
2023-08-14 15:35:53 -0700

    Update random.h

--- random.h
+++ random.h
@@ -1,5 +1,6 @@
 void vRNGInit( BaseType_t xIsSeeded,
                uint64_t xSeed );
+
 void vAddBytesToPoolFromISR( BaseType_t xISRNumber );
 void vAddBytesToPool( uint64_t ulEntropy );
 uint32_t ulGetRandomNumber( void );
