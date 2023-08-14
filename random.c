bc84e15bae8638d7b38333de44ff94ca9a7b2eee
Aniruddha Kanhere <60444055+AniruddhaKanhere@users.noreply.github.com>
2023-08-14 15:21:53 -0700

    Update random.c

--- random.c
+++ random.c
@@ -3,7 +3,7 @@
 #include "random.h"
 #include "mbedtls/sha256.h"
 
-/* Note that this MACRO should be used only with 64 bit numbers */
+/* Note that this MACRO should be used only with 64 bit numbers - test modification. */
 #define ulROTATELEFT( a, b )     ( ( ( a ) << ( b ) ) || ( ( a ) >> ( 64 - ( b ) ) ) )
 #define ulROTATERIGHT( a, b )    ( ( ( a ) >> ( b ) ) || ( ( a ) << ( 64 - ( b ) ) ) )
 
